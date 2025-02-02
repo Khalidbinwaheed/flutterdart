12601e3458b39d7aca7f4783f0c462ee25b15d4f
Khalid Bin Waheed <captainkhalid20@gmail.com>
2025-02-02 19:03:53 +0500

    Update task5.dart

--- Assignment 4/ifelse/task5.dart
+++ Assignment 4/ifelse/task5.dart
@@ -1,2 +1,14 @@
 //Assignment 4
-//Q5. 
+//Q5. Write a program that checks if a number is divisible by both 3 and 5.
+// Example
+import 'dart:io';
+
+void main() {
+  print("Enter a number: ");
+  var number = int.parse(stdin.readLineSync()!);
+  if (number % 3 == 0 && number % 5 == 0) {
+    print("The number is divisible by both 3 and 5");
+  } else {
+    print("The number is not divisible by both 3 and 5");
+  }
+}
