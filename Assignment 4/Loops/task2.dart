a9895e106773a201ebf20141b65a9f1c52f88bea
Khalid Bin Waheed <captainkhalid20@gmail.com>
2025-02-02 19:34:18 +0500

    Create task2.dart

---
+++ Assignment 4/Loops/task2.dart
@@ -0,0 +1,11 @@
+//Assignment 4
+// Q2. Write a program that finds the sum of all numbers from 1 to 100 using a while loop.
+// Example
+void main() {
+  int i = 1, sum = 0;
+  while (i <= 100) {
+    sum += i;
+    i++;
+  }
+  print("Sum of numbers from 1 to 100 is $sum");
+}
